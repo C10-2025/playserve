fca841b06f23e453eccb6132e884811fea714173
Jonathan Yitskhaq Rundjan <jonathanrundjan@Ishaks-Laptop.local>
2025-10-01 16:35:04 +0700

    Done

--- PlayServe/urls.py
+++ PlayServe/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for padelin project.
+URL configuration for playserve project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/

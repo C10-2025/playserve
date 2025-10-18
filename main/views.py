7bc79318774303486728b135fb66fa92ee70e558
unknown <annisa.fakhira17@gmail.com>
2025-10-18 19:20:34 +0700

    rapihin landing page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from profil.models import Profile
 
 def main_view(request):
-    return render(request, 'main.html')
+    context = {}
+    if request.user.is_authenticated:
+        try:
+            context['profile'] = request.user.profile
+        except Profile.DoesNotExist:
+            context['profile'] = None
+            
+    return render(request, 'main.html', context)

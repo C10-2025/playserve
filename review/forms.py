daf0b05175d87e08ad81b8e3afc4e6e714523b45
aiemusles <marcoimanuel06@gmail.com>
2025-10-24 15:24:01 +0700

    Revised, admin pending

--- review/forms.py
+++ review/forms.py
@@ -1,7 +1,8 @@
-from django.forms import ModelForm, CharField, ChoiceField, PasswordInput, ValidationError
+from django.forms import ModelForm
 from review.models import Review
+
 
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = ['nama_lapangan','rating', 'komentar'] 
+        fields = ['lapangan', 'rating', 'komentar']
--- review/models.py
+++ review/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from main.models import Lapangan
 
 """
 Mending ini reviewnya bikin ada tombol tambah review -> kasih form yang minta nama court +
 semua lainnya yg ada di figma -> tampilin semua komentar yang ud dibuat di review_list.html
 """
 class Review(models.Model):
-    nama_lapangan = models.CharField(max_length=255)
+    lapangan = models.ForeignKey(Lapangan, on_delete=models.CASCADE, null=True, blank=True)
     rating = models.PositiveSmallIntegerField(default=0)
     komentar = models.TextField()
+
+    def __str__(self):
+        return f"Review for {self.lapangan.nama if self.lapangan else 'Unknown'} - {self.rating}"

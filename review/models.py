e15d42f94a2b67c830f36cefcfc175b51e005b4c
aiemusles <marcoimanuel06@gmail.com>
2025-10-24 21:20:21 +0700

    Court Reviews and Review Management FINAL

--- review/models.py
+++ review/models.py
@@ -1,11 +1,11 @@
 import uuid
 from django.db import models
-from main.models import Lapangan
+from booking.models import PlayingField
 
 class Review(models.Model):
-    lapangan = models.ForeignKey(Lapangan, on_delete=models.CASCADE, null=True, blank=True)
+    field = models.ForeignKey(PlayingField, on_delete=models.CASCADE, null=True, blank=True)
     rating = models.PositiveSmallIntegerField(default=0)
     komentar = models.TextField()
 
     def __str__(self):
-        return f"Review for {self.lapangan.nama if self.lapangan else 'Unknown'} - {self.rating}"
+        return f"Review for {self.field.nama if self.field else 'Unknown'} - {self.rating}"
--- review/urls.py
+++ review/urls.py
@@ -6,6 +6,6 @@ app_name = 'review'
 urlpatterns = [
     path("add-review/", add_review, name='add_review'),
     path("", review_list, name='review_list'),
-    path("lapangan/<int:lapangan_id>/comments/", view_comments, name='view_comments'),
-    path('review/delete-review/<int:review_id>/', delete_review, name='delete-review'),
+    path("field/<int:field_id>/comments/", view_comments, name='view_comments'),
+    path("delete-review/<int:review_id>/", delete_review, name='delete-review'),
 ]

4289c0bf2a995fe199a3db0663494cdcb68691c1
aiemusles <marcoimanuel06@gmail.com>
2025-11-19 15:41:32 +0700

    Last working version with changes accroding to the recommedations by Nevin, put this here first because the sorting logic might mess up the rest of the module

--- review/models.py
+++ review/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from booking.models import PlayingField
 
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True) # null and blank temp
     field = models.ForeignKey(PlayingField, on_delete=models.CASCADE)
     rating = models.IntegerField()
     komentar = models.TextField()

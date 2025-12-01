b42b1cd36e987cca26d2651c6d56dd86a3d4bbd9
Delta-K-rist <deltakrist.k@gmail.com>
2025-12-01 16:29:38 +0700

    merging with working feat/profil for authentication and login logout

--- playserve/urls.py
+++ playserve/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', include('profil.urls')),
     path('booking/', include('booking.urls')),
-    path('booking/', include('booking.urls')),
     path('community/', include('community.urls')),
     path('matchmaking/', include('matchmaking.urls')),
     path('review/', include('review.urls')),
@@ -35,5 +34,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
